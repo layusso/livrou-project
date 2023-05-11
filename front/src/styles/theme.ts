73af259c5ea27d7d331d0e72eeaeaabbbefced3f
kvitorr <kvitorrsantos@gmail.com>
2023-05-10 23:18:28 -0300

    feat: Create theme.ts and GlobalStyle.ts files
    This commit adds the `theme.ts` and `GlobalStyle.ts` files to the project. The `theme.ts` file contains the theme variables and configurations used throughout the project, making it easier to manage and update the styling. The `GlobalStyle.ts` file contains the global CSS styles applied to the entire project, such as resetting default styles and setting font styles. These files will help to organize and streamline the styling of the project, making it more efficient and maintainable.

---
+++ front/src/styles/theme.ts
@@ -0,0 +1,27 @@
+export const theme = {
+    background: {
+        color: {
+            primary: '#1A2B88',
+            secundary: '#DCE0F9',
+            terciary: '#C9C7C7',
+            
+            neutral: '#FFFFFF',
+            border: '#B3DBDB',
+            input: '#ECEEF6',
+        },
+
+        box: {
+            shadow: '#1A2B88'
+        }
+    },
+
+        font: {
+            weight: {
+                bold: 700,
+                medium: 500,
+                reegular: 400,
+                light: 300     
+            }
+        }
+}
+
