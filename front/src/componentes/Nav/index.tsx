7ac953634c91445b2d4db3fd440b2055ac1ff099
kvitorr <kvitorrsantos@gmail.com>
2023-05-11 01:19:28 -0300

    feat: Create Nav component and style file
    This commit edits the Nav component and its corresponding style file to the project. The Nav component contains the profile of a person and a nav tag with icons and details.

--- front/src/componentes/Nav/index.tsx
+++ front/src/componentes/Nav/index.tsx
@@ -1,7 +1,53 @@
 import React from 'react'
+import { NavWrapper } from './styles'
+import { MdOutlineSpaceDashboard, MdOutlinePostAdd } from 'react-icons/Md';
+import { AiOutlineEllipsis } from 'react-icons/ai'
+import { IoLogOutOutline, IoSettingsSharp } from 'react-icons/io5';
+import { VscPreview } from 'react-icons/vsc'
+import adeliaIcon from '/images/adelia.png'
 
 export const Nav = () => {
   return (
-    <div>Nav</div>
+    <NavWrapper>
+      <ul className='ulNav'>
+        <li className='navOption'>
+          <div className='iconOption'>
+            <MdOutlineSpaceDashboard size={24}/>
+          </div>
+          <p>Home</p>
+        </li>
+        <li className='navOption'>
+          <div className='iconOption'>
+            <VscPreview size={24}/>
+          </div>
+          <p>Reviews</p>
+        </li>
+        <li className='navOption'>
+          <div className='iconOption'>
+            <MdOutlinePostAdd size={24}/>
+          </div>
+          <p>Criar Anúncio</p>
+        </li>
+        <li className='navOption'>
+          <div className='iconOption'>
+            <IoLogOutOutline size={24}/>
+          </div>
+          <p>Logout</p>
+        </li>
+        <li className='navOption'>
+          <div className='iconOption'>
+            <IoSettingsSharp size={22}/>
+          </div>
+          <p>Configurações</p>
+        </li>
+      </ul>
+
+      <div className='profile'>
+        <img src={adeliaIcon} alt="adelia" />
+        <p>Adelia</p>
+        <AiOutlineEllipsis/>
+      </div>
+
+    </NavWrapper>
   )
 }
