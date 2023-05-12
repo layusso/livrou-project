b30c2e2d442a4b36f9ba64a720c2cdce58ad9a3c
kvitorr <kvitorrsantos@gmail.com>
2023-05-12 00:59:51 -0300

    refactor: Modify Header and SearchBar component and style files
    Fixed page response via css changes

--- front/src/componentes/Header/styles.ts
+++ front/src/componentes/Header/styles.ts
@@ -2,21 +2,36 @@ import { styled } from "styled-components";
 import { theme } from "../../styles/theme";
 
 export const HeaderWrapper = styled.header`
-    display: flex;
-    align-items: center;
-    padding: .3rem 1rem;
-
+    width: 100%;
     border-bottom: 1px solid ${theme.background.color.border};
-    
+
+    .HeaderContainer {
+        display: block;
+        max-width: 1000px;
+    }
+
     .imgSearchWrapper {
-        width: 900px;
+        height: 56px;
+        padding: 0 16px;
 
         display: flex;
+        flex-direction: row;
         align-items: center;
         justify-content: space-between;
+        gap: 3rem;
+    }
+
+    .menuImgWrapper {
+        display: flex;
+        align-items: center;
+        gap: .5rem;
+    }
+
+    .menuImgWrapper > * {
+        cursor: pointer;
     }
 
     .livrouLogo {
-        max-width: 150px;
+        max-width: 100px;
     }
 `
