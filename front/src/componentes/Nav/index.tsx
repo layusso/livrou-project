687660241ed89d2e40e993ea9bea451e5e64779b
kvitorr <kvitorrsantos@gmail.com>
2023-05-13 23:19:44 -0300

    refactor: Modify Nav component and style files

--- front/src/componentes/Nav/index.tsx
+++ front/src/componentes/Nav/index.tsx
@@ -1,15 +1,12 @@
 import { NavWrapper } from './styles'
 import { MdOutlineSpaceDashboard, MdOutlinePostAdd, MdKeyboardArrowDown } from 'react-icons/Md';
-import { AiOutlineEllipsis } from 'react-icons/ai'
 import { IoLogOutOutline, IoSettingsSharp } from 'react-icons/io5';
 import { VscPreview } from 'react-icons/vsc'
-import { BsFilter } from 'react-icons/bs'
-import adeliaIcon from '/images/adelia.png'
 
 export const Nav = () => {
   return (
     <NavWrapper>
-      <div>
+      
         <ul className='ulNav'>
           <li className='navOption'>
             <div className='iconOption'>
@@ -29,41 +26,15 @@ export const Nav = () => {
             </div>
             <p>Criar Anúncio</p>
           </li>
-       
         </ul>
 
-        <ul>
-          <li className='filterStyle'>
+        
+          <div className='filterOption'>
             <p>Filtrar</p>
             <div className='iconOption'>
               <MdKeyboardArrowDown size={24}/>
             </div>
-          </li>
-        </ul>
-    
-
-        <ul className='ulNav'>
-          <li className='navOption'>
-            <div className='iconOption'>
-              <IoLogOutOutline size={24}/>
-            </div>
-            <p>Logout</p>
-          </li>
-          <li className='navOption'>
-            <div className='iconOption'>
-              <IoSettingsSharp size={21}/>
-            </div>
-            <p>Configurações</p>
-          </li>
-        </ul>
-      </div>
-
-      <div className='profile'>
-        <img src={adeliaIcon} alt="adelia" />
-        <p>Adelia</p>
-        <AiOutlineEllipsis/>
-      </div>
-
+          </div>
     </NavWrapper>
   )
 }
