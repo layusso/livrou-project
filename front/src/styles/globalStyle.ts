1bbf2f00d442f66678ecfa05528fbad76920a5d8
kvitorr <kvitorrsantos@gmail.com>
2023-05-16 18:41:10 -0300

    style: change font-family

--- front/src/styles/globalStyle.ts
+++ front/src/styles/globalStyle.ts
@@ -5,7 +5,7 @@ export const GlobalStyle = createGlobalStyle`
         padding: 0;
         margin: 0;
         box-sizing: border-box;
-        font-family: 'Poppins', sans-serif;
+        font-family: 'Roboto', sans-serif;
     }
 
     p {
